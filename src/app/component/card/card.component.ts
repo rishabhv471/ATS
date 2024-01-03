28738f20bf0a7233f02e36487a7002f50efb5cd6
rishabhv471 <rishabhv471@gmail.com>
2024-01-03 21:27:12 +0530

    Add existing project files to Git

--- src/app/component/card/card.component.ts
+++ src/app/component/card/card.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
 
+  card = { title: 'Card Title', 
+           text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+           live_position:'10',
+           Experience:'2-3',
+           salary:'usd 1000-2000',
+           Domain:'IT',
+           Contact_email:'xyz@gmail.com',
+           btnText: 'Go somewhere' 
+         };
+         
+  selectedRating = 0;
+  stars = [
+            {
+              id: 1,
+              icon: 'star',
+              class: 'star-gray star-hover star'
+            },
+          ];
+
+    card_display= true;
+
+    selectStar(value: number): void{  
+      if ( this.selectedRating === 0){
+        this.stars.filter( (star) => {
+          if ( star.id <= value){
+            star.class = 'star-gold star';
+          }else{ 
+           star.class = 'star-gray star'; 
+          } 
+          return star;
+        });  
+      }  
+      this.selectedRating = value;
+    }
+
+    ShowLiveJobs(){
+      this.card_display = true;
+      console.log(this.card_display);
+    }
 }
+
+
+
